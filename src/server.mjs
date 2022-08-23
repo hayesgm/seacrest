6565bad2c218fe7f584af7177f494a8d98d6579d
Kevin Cheng <kevincheng96@hotmail.com>
2022-08-23 10:50:05 -0700

    Revert latest change

--- src/server.mjs
+++ src/server.mjs
@@ -46,8 +46,9 @@ function getReqBody(req) {
 
 async function proxy(host, request, reqBody, response) {
   let pathname = url.parse(request.url).pathname;
+  pathname = pathname === "/" ? "" : pathname; // to avoid a trailing slash
   let hostname = url.parse(host).host;
-  let proxyUrl = `${host}${pathname}`.replace(/\/+$/, ""); // remove trailing slashes
+  let proxyUrl = `${host}${pathname}`;
   let reqMethod = request.method;
   let reqHeaders = {
     ...request.headers,
