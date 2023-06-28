e2b77e06a9aa1d2457f88a50c580962ab0a80aac
Geoffrey Hayes <hayesgm@gmail.com>
2023-06-28 12:26:04 -0700

    Change param order

--- src/index.mjs
+++ src/index.mjs
@@ -2,26 +2,27 @@
 ":" //# comment; exec /usr/bin/env node --noharmony "$0" "$@"
 import { startServer } from "./server.mjs";
 
-let host = process.argv[2] || process.env["ETHEREUM_URL"] || 'https://mainnet.infura.io';
-if (!host.startsWith("http")) {
-  throw new Error(`Expected host to start with http, got: ${host}`);
-}
-const portStr = process.argv[3] || process.env["PORT"] || 8585;
-const port = Number(portStr);
-if (Number.isNaN(port)) {
-  throw new Error(`Invalid port ${portStr}`);
-}
-let requestedNetwork = process.argv[4] || process.env["REQUESTED_NETWORK"];
-if (!Number.isNaN(requestedNetwork)) {
+let requestedNetwork = process.argv[2] || process.env["REQUESTED_NETWORK"];
+if (!Number.isNaN(Number(requestedNetwork))) {
   requestedNetwork = Number(requestedNetwork);
 }
 if (!requestedNetwork) {
   throw new Error(`Missing required REQUESTED_NETWORK`);
 }
 
-const walletConnectProjectId = process.argv[5] || process.env["WALLET_CONNECT_PROJECT_ID"];
+const walletConnectProjectId = process.argv[3] || process.env["WALLET_CONNECT_PROJECT_ID"];
 if (!walletConnectProjectId) {
   throw new Error(`Missing required WALLET_CONNECT_PROJECT_ID`);
+}
+
+let host = process.argv[4] || process.env["ETHEREUM_URL"] || 'https://mainnet.infura.io';
+if (!host.startsWith("http")) {
+  throw new Error(`Expected host to start with http, got: ${host}`);
+}
+const portStr = process.argv[5] || process.env["PORT"] || 8585;
+const port = Number(portStr);
+if (Number.isNaN(port)) {
+  throw new Error(`Invalid port ${portStr}`);
 }
 
 let connectOpts = {};
